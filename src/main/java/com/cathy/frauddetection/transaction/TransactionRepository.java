5af714ad54e1362bda169d336ebdebb270a51ce4
Cathy0326 <cathyyue0326@gmail.com>
2026-09-08 13:28:54 +0100

    feat: enable Specification queries on TransactionRepository

--- src/main/java/com/cathy/frauddetection/transaction/TransactionRepository.java
+++ src/main/java/com/cathy/frauddetection/transaction/TransactionRepository.java
@@ -1,9 +1,11 @@
 package com.cathy.frauddetection.transaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 // Package-private: nothing outside this package can reach the database directly.
-interface TransactionRepository extends JpaRepository<Transaction, Long> {
+interface TransactionRepository extends JpaRepository<Transaction, Long>,
+        JpaSpecificationExecutor<Transaction> {
 
     // Derived query: Spring Data parses the method name into
     // SELECT EXISTS(... WHERE transaction_ref = ?) at startup.
